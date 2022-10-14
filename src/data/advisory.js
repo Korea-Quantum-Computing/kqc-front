e9f616f656f6d24815abb6cd8aa6eed6500d738e
Kion Kim <kion.kim@gmail.com>
2022-10-14 13:04:13 +0900

    moved advisory information to data/advisory.json

---
+++ src/data/advisory.js
@@ -0,0 +1,44 @@
+export const advisors = [
+    {
+      name: 'Sungsu Park',
+      title: 'Head of ....',
+      company: 'ETRI',
+      image: 'sungsoo-park.jpeg',
+      desc: 'desc here desc here  blabla'
+    },
+    {
+      name: 'Hakbae Lee',
+      title: 'Professor, Statstics & Datascience',
+      image: 'sungsoo-park.jpeg',
+      company: 'Yonsei University',
+      desc: 'He is a professor blabla'
+    },
+    {
+      name: 'InKweon Chung',
+      title: 'Professor, Statstics & Datascience',
+      image: 'sungsoo-park.jpeg',
+      company: 'Yonsei University',
+      desc: 'He is a professor blabla'
+    },
+    {
+      name: 'Gabyoung Chung',
+      title: 'Professor, Economics',
+      image: 'sungsoo-park.jpeg',
+      company: 'Yonsei University',
+      desc: 'He is a professor blabla'
+    },
+    {
+      name: 'Daniel. K. Park',
+      title: 'Professor, Statstics & Datascience',
+      image: 'sungsoo-park.jpeg',
+      company: 'Yonsei University',
+      desc: 'He is a professor blabla'
+    },
+    {
+      name: 'Shin Cho',
+      title: 'Professor, Statstics & Datascience',
+      image: 'sungsoo-park.jpeg',
+      company: 'Yonsei University',
+      desc: 'He is a professor blabla'
+    },
+  ];
