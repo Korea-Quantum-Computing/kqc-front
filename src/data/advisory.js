4899e68367d138fdf7cfb8fc8461d967947f799f
seungwoo-kqc <seungwoo.cho@kqchub.com>
2022-10-28 14:59:23 +0900

    update advisory name

--- src/data/advisory.js
+++ src/data/advisory.js
@@ -8,7 +8,7 @@ export const advisors = [
     desc: 'As an expert of Dimension Reduction and Statistical Linear Model, he have studied the application of quantum computing at the data science. He worked as a Dean of Yonsei Graduate School of Economics'
    },
    {
-    name: 'Gabyoung Chung',
+    name: 'Kap-Young Chung',
     title: 'Emeritus Professor',
     image: 'Gabyoung-Chung.jpg',
     company: 'Yonsei University - Department of Economics',
@@ -16,13 +16,21 @@ export const advisors = [
     desc: 'He is working as president of the Korean committee for UNICEF. He served as the former president of Yonsei University and the former managing director at International Economic Association.'
    },
    {
-    name: 'InKweon Chung',
+    name: 'In Kwon Chung',
     title: 'Professor',
     image: 'InKweon-Chung.JPG',
     company: 'Yonsei University - Department of Systems Biology',
     degree: 'PhD (Biology, Ohio State University)',
     desc: 'He have studied the telomere and telomerase biology. He served as head of department of Integrated OMICS for Biomedical Science.'
    },
+   {
+    name: 'Seong Su Park',
+    title: 'Assistant Vice President',
+    company: 'ETRI - Quantum Technology Research Department',
+    image: 'Sungsu-Park.jpeg',
+    degree: 'PhD (Materials science & engineering, Korea Institute of Science and Technology)',
+    desc: 'He have worked as director of quantum technology research department of ETRI. He is designated as Convenor of Standard Evaluation Group 14 (Quantum Information) of IEC'
+  },
    {
     name: 'Shin Cho',
     title: 'Professor',
@@ -32,19 +40,11 @@ export const advisors = [
     desc: 'He served as Senior Secretary for Future Strategy of the Blue House and worked as Chief Executeve Officer at SK broadband. He have studied economics of technological innovation.'
    },
    {
-    name: 'Daniel. K. Park',
+    name: 'Daniel Kyungdeock Park',
     title: 'Assistant Professor',
     image: 'Daniel-K-Park.jpg',
     company: 'Yonsei University - Department of Statstics & Datascience',
     degree: 'PhD (Physics & Quantum Information, University of Waterloo)',
     desc: 'He is researching Quantum Neural Network and expanding it into various data science fields including anomaly detection and image recognition. He studied at the Institue for Quantum Computing at University of Waterloo'
-   },
-   {
-    name: 'Seong Su, Park',
-    title: 'Assistant Vice President',
-    company: 'ETRI - Quantum Technology Research Department',
-    image: 'Sungsu-Park.jpeg',
-    degree: 'PhD (Materials science & engineering, Korea Institute of Science and Technology)',
-    desc: 'He have worked as director of quantum technology research department of ETRI. He is designated as Convenor of Standard Evaluation Group 14 (Quantum Information) of IEC'
-  }
+   }
  ];
