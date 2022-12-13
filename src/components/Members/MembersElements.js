c91f35f716ff3f7e22d23e20645a15e66acc39d0
seungwoo-kqc <seungwoo.cho@kqchub.com>
2022-12-13 09:55:45 +0900

    memberspace_height_adjust

--- src/components/Members/MembersElements.js
+++ src/components/Members/MembersElements.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 
 export const MembersContainer = styled.div`
-  min-height: 850px;
+  min-height: 1000px;
   display: flex;
   flex-direction: column;
   // justify-content: start;
